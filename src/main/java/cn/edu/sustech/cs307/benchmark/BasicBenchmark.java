79f859989227a9d41f53b27882993ea38607d53d
yuki <chenjf2020@mail.sustech.edu.cn>
2022-09-13 15:46:10 +0800

    remove unused BasicBenchmark

--- src/main/java/cn/edu/sustech/cs307/benchmark/BasicBenchmark.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.sustech.cs307.benchmark;
-
-import cn.edu.sustech.cs307.config.Config;
-import cn.edu.sustech.cs307.factory.ServiceFactory;
-
-public abstract class BasicBenchmark {
-    protected ServiceFactory serviceFactory = Config.getServiceFactory();
-}
